ed48c6fe7c84f9e625922b123de80332d0725add
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-22 18:20:05 +0900

    Title: 배열 채우기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/배열 채우기/filling-array.py
+++ 240122/배열 채우기/filling-array.py
@@ -4,7 +4,7 @@ reversed_arr = arr[::-1]
 
 
 for i in range(len(reversed_arr)):
-    if(reversed_arr[i] == 0):
+    if(i == 0 and reversed_arr[i] == 0):
         continue
     else:
         if(reversed_arr[i] == 0):
