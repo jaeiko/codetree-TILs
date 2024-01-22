d8dd80f6fdfaf399eb0e08cd74913221d6aa186b
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-22 18:07:02 +0900

    Title: 학점 계산기, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/학점 계산기/credit-calculator.py
@@ -0,0 +1,10 @@
+n = int(input())
+score_arr = list(map(float, input().split()))
+
+print(f'{sum(score_arr) / n:.1f}')
+if(sum(score_arr) / n >= 4.0):
+    print('Perfect')
+elif(sum(score_arr) >= 3.0):
+    print('Good')
+else:
+    print('Poor')
