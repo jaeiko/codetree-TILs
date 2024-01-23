2c19d803cb23f3253cfdeba74837089c5604bddb
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-24 01:25:33 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240124/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240124/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,16 +1,16 @@
+# 숫자, 리스트에 넣을 정수 입력
 n = int(input())
 arr = list(map(int, input().split()))
+
+# 인덱스 값 넣을 리스트
 index_arr = list()
 
-i = 1
 while True:
-    index_arr.append(arr.index(max(arr)) + 1)
-    if arr.index(max(arr)) == 0:
+    index_arr.append(arr.index(max(arr)) + 1)   # 리스트 중 가장 최댓값 인덱스 입력
+    if arr.index(max(arr)) == 0:    # 그 최댓값 인덱스가 0이면 다 찾은거니 탈출
         break
-    arr = arr[arr.index(max(arr))-i::-1]
-    i += 1
+    arr = arr[0:arr.index(max(arr))]    # 인덱스 0~최댓값 있는 인덱스-1까지의 리스트를 arr에 저장
 
-
-
-for elem in set(index_arr):
+# 요소 출력
+for elem in index_arr:
     print(elem, end=' ')
