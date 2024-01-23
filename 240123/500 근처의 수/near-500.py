eac9e04c6db83d3eb9a3428535dbf507ba982d8d
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-23 16:24:11 +0900

    Title: 500 근처의 수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/500 근처의 수/near-500.py
+++ 240123/500 근처의 수/near-500.py
@@ -2,18 +2,18 @@ import sys
 arr = list(map(int, input().split()))
 
 # 500 미만의 수 중 가장 큰 수 찾는 변수
-sub_val1 = abs(arr[0] - 500)
+sub_val1 = abs(min(arr) - 500)
 max_val = 0
 
 # 500 미만의 수 중 가장 작은 수 찾는 변수
-sub_val2 = abs(arr[0] - 500)
+sub_val2 = abs(max(arr) - 500)
 min_val = 0
 
 for i in range(1, 10):
     if arr[i] - 500 < 0 and abs(arr[i] - 500) < sub_val1:  # 500 미만의 수 중 가장 큰 수 찾는 코드
         sub_val1 = abs(arr[i] - 500)
         max_val = arr[i]
-    if arr[i] - 500 > 0 and abs(arr[i] - 500) < sub_val2:  # 500 초과의 수 중 가장 작은 수를 찾는 코드
+    elif arr[i] - 500 > 0 and abs(arr[i] - 500) < sub_val2:  # 500 초과의 수 중 가장 작은 수를 찾는 코드
         sub_val2 = abs(arr[i] - 500)
         min_val = arr[i]
 
