b35817ce02478116bf7da1cd3eebff9580cec759
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-24 00:18:40 +0900

    Title: 배열의 평균, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240124/배열의 평균/ave-of-array.py
@@ -0,0 +1,24 @@
+arr = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+total_sum = 0
+
+for i in range(2):
+    row_sum_val = 0
+    for j in range(4):
+        row_sum_val += arr[i][j]
+    print(row_sum_val / 4, end=' ')
+
+print()
+
+for i in range(4):
+    column_sum_val = 0
+    for j in range(2):
+        column_sum_val += arr[j][i]
+        total_sum += arr[j][i]
+    print(column_sum_val / 2, end=' ')
+
+print()
+print(total_sum / 8)
