143afd3cb385c9bba876425f580b390973006e1c
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-24 00:26:26 +0900

    Title: 두 숫자의 차의 최솟값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240124/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 240124/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -2,10 +2,10 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 min_val = arr[1] - arr[0]
-for i in range(len(arr)):
+for i in range(1, n):
     if i == 4:
         break
-    if(arr[i+1] - arr[i] < min_val):
+    if arr[i+1] - arr[i] < min_val:
         min_val = arr[i+1] - arr[i]
 
 print(min_val)
