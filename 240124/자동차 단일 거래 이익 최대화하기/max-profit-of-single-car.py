348f15fc68f3e6145d7fbc583f270e4b80878e04
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-24 00:28:26 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240124/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+price_arr = list(map(int, input().split()))
+
+difference = price_arr[1] - price_arr[0]
+
+if n == 1:
+    print(0)
+else:
+    for i in range(n):
+        for j in range(i + 1, n):
+            if j == n:
+                break
+            if price_arr[j] - price_arr[i] > difference:
+                difference = price_arr[j] - price_arr[i]
+    print(difference)
