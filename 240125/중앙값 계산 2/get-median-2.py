f01dc5297e8ea16be9acce1557bbefd5b8070fde
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-25 00:38:50 +0900

    Title: 중앙값 계산 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/중앙값 계산 2/get-median-2.py
+++ 240125/중앙값 계산 2/get-median-2.py
@@ -1,9 +1,10 @@
 # 입력할 요소 개수 n 입력
 n = int(input())
 
-# 요소 입력해서 정렬한 뒤, arr 리스트에 저장(중앙값 찾기 위해 미리 오름차순으로 정렬)
-arr = sorted(list(map(int, input().split())))
+# 요소 입력해서 arr 리스트에 저장
+arr = list(map(int, input().split()))
 
 for i in range(n):
     if (i+1) % 2 != 0:  # 홀수 번째 수 일 때
-        print(arr[i // 2], end=' ') # 중앙값 출력
+        sorted_arr = sorted(arr[0:i+1])    # 중앙값을 구하기 위해 0 ~ i번째 요소들을 오름차순으로 정렬하여 sorted_arr 에 저장
+        print(sorted_arr[i // 2], end=' ') # 중앙값 출력
