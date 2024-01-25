61fb9b3c455f9e12272a73e954479b21dfa7bd40
Kim JaeJun <88999549+jaeiko@users.noreply.github.com>
2024-01-25 10:37:08 +0900

    Title: 2개씩 그룹짓기, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/2개씩 그룹짓기/group-of-pairs.py
+++ 240125/2개씩 그룹짓기/group-of-pairs.py
@@ -12,19 +12,21 @@ arr = sorted(list(map(int, input().split())))
 pre_sum_val = 0
 
 # 처음에 중앙의 값들을 더하기 위해 인덱스 i, j 설정
-i = len(arr) // 2 - 1
+i = (len(arr) // 2) - 1
 j = (len(arr) // 2)
 
 while True:
-    if i == -1 or j == len(arr) - 1: #만약 인덱스 i 가 0 아래에 있거나 인덱스 j가 리스트의 끝을 넘는다면 반복문 종료 후 pre_sum_val 출력
+    # 만약 인덱스 i 가 0 아래에 있거나 인덱스 j가 리스트의 끝을 넘는다면 반복문 종료 후 pre_sum_val 출력
+    if i <= -1 or j >= len(arr):
         break
-    if i == len(arr) // 2 - 1:  # 맨 처음 pre_sum_val 설정하기 위해 정렬된 리스트의 가장 중앙 2개값을 더한 값으로 초기값 설정
+    if i == (len(arr) // 2) - 1:  # 맨 처음 pre_sum_val 설정하기 위해 정렬된 리스트의 가장 중앙 2개값을 더한 값으로 초기값 설정
         pre_sum_val = arr[i] + arr[j]
-    else:   # 점차 arr[i]과 arr[j]가 양쪽으로 가면서 더하여 sum_val에 저장. pre_sum_val보다 크다면 갱신.
+    # 점차 arr[i]과 arr[j]가 양쪽으로 가면서 더하여 sum_val에 저장. pre_sum_val보다 크다면 갱신.
+    else:
         sum_val = arr[i] + arr[j]
         if pre_sum_val < sum_val:
             pre_sum_val = sum_val
-    
+
     # 양쪽으로 가기 위해 i 는 -1을, j는 +1을 더함
     i -= 1
     j += 1
