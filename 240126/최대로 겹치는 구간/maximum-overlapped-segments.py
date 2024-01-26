d7c9ea340f6b25d1129846517786185e189fd272
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-26 11:26:54 +0900

    Title: 최대로 겹치는 구간, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240126/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,13 +1,14 @@
 # 입력 횟수 n
 n = int(input())
 
+OFFSET = 100    # OFFSET 100으로 설정(음수가 아닌 0 이상의 범위로 정하기 위해)
+
 line = [0] * 200  # x1:0 ~ x2:200 의 선분
 
 for i in range(n):
     x1, x2 = tuple(map(int, input().split()))   # 겹칠 구간 x1~x2 입력
-    if x1 < 0 : # 만약 x1이 음수이면 그 x1만큼 절댓값을 더해서 양수로 설정
-        x1 += abs(x1)
-        x2 += abs(x1)
+    x1 += OFFSET
+    x2 += OFFSET
     for j in range(x1, x2): # 겹치는 구간에 1증가
         line[j] += 1
 
