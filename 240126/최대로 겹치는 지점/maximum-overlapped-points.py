a08d29250cb35c070f233f97b2845569a99401e0
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-26 11:38:44 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240126/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,12 @@
+# 반복 횟수 n
+n = int(input())
+
+line = [0] * 101    # 1~100
+
+for _ in range(n):
+    x1, x2 = tuple(map(int, input().split()))   # x1, x2 입력
+    for i in range(x1, x2+1):   x1~x2의 지점에 1증가
+        line[i] += 1
+
+가장 많이 겹치는 선분의 지점 출력
+print(max(line))
