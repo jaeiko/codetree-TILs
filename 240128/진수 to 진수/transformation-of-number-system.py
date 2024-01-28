85cf764881c4d6d674b7c79d45fe0664c2631718
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-28 19:59:51 +0900

    Title: 진수 to 진수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240128/진수 to 진수/transformation-of-number-system.py
+++ 240128/진수 to 진수/transformation-of-number-system.py
@@ -1,12 +1,18 @@
 '''
 정수 a와 b가 주어지고, a진수로 표현된 어떤 수 n이 주어지면, n을 b진수로 변환하여 출력하는 프로그램
 '''
+import sys
 
 # n의 진수 a와 n을 변환할 진수 b 입력
 a, b = tuple(map(int, input().split()))
 
 # a진수로 표현된 n 입력
 n = list(map(int, input()))
+
+if a == b:  # a와 b가 같으면 진수가 같으니 그대로 n 출력하고 시스템 종료
+    for elem in n:
+        print(elem, end='')
+    sys.exit(0)
 
 # a진수로 표현된 n을 일단 10진수로 변환
 decimal = 0
