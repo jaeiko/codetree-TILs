30411a04b4ebe70473d5d72a0e0f2e733b829e66
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-29 15:58:01 +0900

    Title: 왔다 갔던 구역 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240129/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240129/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -17,19 +17,12 @@ for _ in range(n):
     x, command = tuple(map(str, input().split()))   # 명령 입력
 
     if command == 'L':      # 왼쪽 이동이면 이전에 왼쪽으로 갔던 곳부터 시작해서 구간을 지나간다.
-        for i in range(left_index, left_index + int(x)):
-            sections[i] += 1
         left_index += int(x)    # left_index에 x+1만큼 증가
     elif command == 'R':    # 오른쪽 이동이면 이전에 오른쪽으로 갔던 곳부터 시작해서 구간을 지나간다.
-        for i in range(right_index, right_index + int(x)):
-            sections[i] += 1
         right_index += int(x)   # right_index에 x+1만큼 증가
 
 # 2번 이상 지나간 구간 구하기
-checked = 0
-for section in sections:
-    if section >= 2:
-        checked += 1
+checked = max(left_index, right_index) - abs(left_index - right_index)
 
 # 출력
 print(checked)
