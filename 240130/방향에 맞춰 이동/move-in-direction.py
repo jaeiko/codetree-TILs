80f0dd9453b1390052ced15371a0dfcbd3a3de41
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-30 10:38:52 +0900

    Title: 방향에 맞춰 이동, Time: 227ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/방향에 맞춰 이동/move-in-direction.py
+++ 240130/방향에 맞춰 이동/move-in-direction.py
@@ -8,15 +8,26 @@ n = int(input())
 # 시작 위치
 x, y = 0, 0
 
+# dir_num : 방향 번호
+dir_num = 0
+
+# dx, dy 테크닉
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+
 for _ in range(n):
     direction, distance = tuple(map(str, input().split()))
     if direction == 'N':    # N이면 y축 방향으로 distance 증가
-        x, y = x, y + int(distance)
+        dir_num = 0
     elif direction == 'S':  # S이면 y 축 방향으로 -distance 증가
-        x, y = x, y - int(distance)
+        dir_num = 1
     elif direction == 'E':  # E이면 x 축 방향으로 distance 증가
-        x, y = x + int(distance), y
+        dir_num = 2
     elif direction == 'W':  # W이면 x 축 방향으로 -distance 증가
-        x, y = x - int(distance), y
+        dir_num = 3
 
+    # 해당 방향으로 x와 y 증가
+    x += int(distance) * dx[dir_num]
+    y += int(distance) * dy[dir_num]
+
+# 출력
 print(x, y)
