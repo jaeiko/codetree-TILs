6188b4e0496e255063c71564a43c9bfb3db243ae
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-30 10:25:31 +0900

    Title: 방향에 맞춰 이동, Time: 225ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/방향에 맞춰 이동/move-in-direction.py
+++ 240130/방향에 맞춰 이동/move-in-direction.py
@@ -2,22 +2,21 @@
 (0, 0)에서 시작하여 총 N번 움직여보려고 한다. N번에 걸쳐 움직이려는 방향과 움직일 거리가 주어졌을 때, 최종 위치를 출력하는 프로그램
 '''
 
+# 움직일 횟수 n
 n = int(input())
 
-# 방향 리스트
-direction_list = ['N', 'S', 'E', 'W']
-
+# 시작 위치
 x, y = 0, 0
 
 for _ in range(n):
     direction, distance = tuple(map(str, input().split()))
-    if direction == 'N':
+    if direction == 'N':    # N이면 y축 방향으로 distance 증가
         x, y = x, y + int(distance)
-    elif direction == 'S':
+    elif direction == 'S':  # S이면 y 축 방향으로 -distance 증가
         x, y = x, y - int(distance)
-    elif direction == 'E':
+    elif direction == 'E':  # E이면 x 축 방향으로 distance 증가
         x, y = x + int(distance), y
-    elif direction == 'W':
+    elif direction == 'W':  # W이면 x 축 방향으로 -distance 증가
         x, y = x - int(distance), y
 
 print(x, y)
