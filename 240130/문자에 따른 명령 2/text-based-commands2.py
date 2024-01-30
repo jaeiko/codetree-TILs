b6e0822a2d99883c9141fb2231a2f0dd7849bf34
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-01-30 11:23:14 +0900

    Title: 문자에 따른 명령 2, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/문자에 따른 명령 2/text-based-commands2.py
+++ 240130/문자에 따른 명령 2/text-based-commands2.py
@@ -1,3 +1,7 @@
+'''
+dx, dy 테크닉을 활용한 문제 2
+'''
+
 # 좌표평면 위 (0, 0)에서 북쪽을 향한 상태
 dir_num = 3 
 x, y = 0, 0
@@ -6,12 +10,14 @@ x, y = 0, 0
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 
 # 입력받은 명령을 하나씩 분리하여 command_list에 저장
-command_list = list(input())
+command = input()
 
-for elem in command_list:
+for elem in command:
 # 회전 방향 : 원래 방향의 90도로 변경
-    if elem == 'L':  # L이면 반시계 방향으로 90도 회전
+    if elem == 'L':     # L이면 반시계 방향으로 90도 방향 전환
         dir_num = (dir_num - 1 + 4) % 4
+    elif elem == 'R':   # R이면 시계 방향으로 90도 방향 전환
+        dir_num = (dir_num + 1) % 4
     elif elem == 'F': # F이면 move
         x += dx[dir_num]
         y += dy[dir_num]
