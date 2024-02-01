54deb228b7403d933caabfe830a6d035f351697c
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-02-02 00:07:05 +0900

    Title: 일렬로 서있는 소 2, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240202/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,23 @@
+'''
+완전탐색 문제
+
+3마리의 서로 다른 소의 위치를 (i, j, k)라고 했을 때, i < j < k를 만족하며 
+동시에 Ai <= Aj <= Ak를 만족하는 서로 다른 쌍의 수를 구하는 프로그램
+'''
+
+# 소 마릿수 n
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+cnt = 0 # 위의 조건을 만족하는 서로 다른 쌍의 수를 저장하는 변수
+
+# 완전 탐색
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if arr[i] < arr[j] < arr[k]:
+                cnt += 1
+
+# 출력
+print(cnt)
