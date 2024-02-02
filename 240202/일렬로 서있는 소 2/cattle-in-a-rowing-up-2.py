df65f07bd676c6ac991277edb0df245e5bb5821f
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-02-02 11:39:35 +0900

    Title: 일렬로 서있는 소 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240202/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -16,7 +16,7 @@ cnt = 0 # 위의 조건을 만족하는 서로 다른 쌍의 수를 저장하는
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if arr[i] < arr[j] < arr[k]:
+            if arr[i] <= arr[j] <= arr[k]:
                 cnt += 1
 
 # 출력
