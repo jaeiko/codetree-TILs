99c941fb33852f6ef76792629a7f06f7bdf56534
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-02-17 23:55:43 +0900

    Title: 두 직사각형, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240217/두 직사각형/two-rectangles.py
@@ -0,0 +1,23 @@
+import sys
+
+'''
+케이스별로 나누기 - 겹치는 경우
+
+2차 평면 상에 두 직사각형이 주어졌을 때, 두 직사각형의 겹쳐져 있는지를 판단하는 프로그램
+'''
+
+
+# (x1, y1), (x2, y2) 값을 공백에 사이를 두고 입력
+x1, y1, x2, y2 = tuple(map(int, input().split()))
+
+# (a1, b1), (a2, b2) 값을 공백에 사이를 두고 입력
+a1, b1, a2, b2 = tuple(map(int, input().split()))
+
+# 두 사각형이 겹치지 않으면 "nonoverlapping" 출력 후 종료
+if x2 < a1 or a2 < x1:
+    if b2 < y1 or y2 < b1:
+        print("nonoverlapping")
+        sys.exit(0)
+
+# 두 사각형이 겹친다면 "overlapping" 출력 후 종료
+print("overlapping")
