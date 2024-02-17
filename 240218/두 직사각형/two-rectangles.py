6e22f0c284fcbbf57fa177ba7c7cfae4346cb307
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-02-18 00:11:17 +0900

    Title: 두 직사각형, Time: 236ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/두 직사각형/two-rectangles.py
+++ 240218/두 직사각형/two-rectangles.py
@@ -14,8 +14,7 @@ x1, y1, x2, y2 = tuple(map(int, input().split()))
 a1, b1, a2, b2 = tuple(map(int, input().split()))
 
 # 두 사각형이 겹치지 않으면 "nonoverlapping" 출력 후 종료
-if x2 < a1 or a2 < x1:
-    if b2 < y1 or y2 < b1:
+if x2 < a1 or a2 < x1 or b2 < y1 or y2 < b1:
         print("nonoverlapping")
         sys.exit(0)
 
