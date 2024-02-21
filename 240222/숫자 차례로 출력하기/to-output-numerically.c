323e531893bc88441a8035f5bc66724f6f6e0352
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-02-22 00:11:47 +0900

    Title: 숫자 차례로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240222/숫자 차례로 출력하기/to-output-numerically.c
+++ 240222/숫자 차례로 출력하기/to-output-numerically.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-# 오름차순으로 출력하는 재귀함수
+// 오름차순으로 출력하는 재귀함수
 void PrintAscendingNum(int n) {
     if (n == 0) {
         return;
@@ -16,7 +16,7 @@ void PrintAscendingNum(int n) {
     printf("%d ", n);
 }
 
-# 내림차순으로 숫자 출력하는 재귀함수
+// 내림차순으로 숫자 출력하는 재귀함수
 void PrintDescendingingNum(int n) {
     if (n == 0) {
         return;
@@ -29,7 +29,7 @@ int main() {
     int n;
     scanf("%d", &n);    # n 입력
 
-    # 함수 호출을 통한 결과값 출력
+    // 함수 호출을 통한 결과값 출력
     PrintAscendingNum(n);
     printf("\n");
     PrintDescendingingNum(n);
