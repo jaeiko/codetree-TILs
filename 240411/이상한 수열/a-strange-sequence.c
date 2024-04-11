a2ff147329288eaa73490cd17f47ae1a9ee279c5
KIM JAEJUN <88999549+jaeiko@users.noreply.github.com>
2024-04-11 15:50:02 +0900

    Title: 이상한 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240411/이상한 수열/a-strange-sequence.c
+++ 240411/이상한 수열/a-strange-sequence.c
@@ -1,8 +1,11 @@
+// 값을 반환하는 재귀함수 문제
+// 첫 번째 수는 1이고, 두 번째 수는 2이고, N 번째 수는 ⌊N/3⌋ 번째 수와 (N-1)번째 수의 합으로 나열된 수열. 
+// 정수 N이 주어지면 재귀호출을 이용하여 이 수열에서 N번째 수를 구하는 프로그램
 #include <stdio.h>
 #include <stdlib.h>
 
 int recursion(int* arr, int index, int search) {
-    if (index == 1 || index == 2) return index; // 찾고자 하는 index의 값이 1이거나 2이면 index(1 또는 2) 반환
+    if (search == 1 || search == 2) return search; // 찾고자 하는 값의 인덱스가 1이거나 2이면 1(2)를 반환
 
     arr[index] = arr[index / 3] + arr[index - 1];   // N 번째 수는 (N/3)번째 수와 (N-1)번째 수의 합으로 나열된 수열
     if (index == search) return arr[index]; // 우리가 찾고자 하는 값에 해당하는 인덱스에 도달했다면 해당 요소 반환
@@ -12,7 +15,7 @@ int recursion(int* arr, int index, int search) {
 int main() {
     int n;  // 정수 n
     scanf("%d", &n);    // n 입력
-    int* arr = malloc(sizeof(int) * (n+1));  // 4byte * n 크기의 동적배열 할당
+    int* arr = malloc(sizeof(int) * (n+5));  // 4byte * n 크기의 동적배열 할당
 
     // 배열의 첫 번째 요소와 두 번째 요소는 각각 1, 2
     arr[0] = 0;
@@ -20,5 +23,8 @@ int main() {
     arr[2] = 2;
 
     printf("%d", recursion(arr, 3, n));  // 인자값으로 배열 arr과 n, n-1(=search:우리가 찾고자 하는 값의 인덱스), 2(배열의 세번째 수에 해당하는 인덱스) 전달해서 계산 후 n번째 수를 출력
+
+    // 할당된 메모리 해제
+    free(arr);
     return 0;
 }
